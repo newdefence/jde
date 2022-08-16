275338649ad6acf4b67af6bac836d8ea8318643f
defence <newdefence@163.com>
2022-08-16 23:54:03 +0800

    文件自验部分完成

--- Taiflex.py
+++ Taiflex.py
@@ -6,7 +6,6 @@ import logging
 from decimal import Decimal
 
 from openpyxl import load_workbook
-from openpyxl.cell.read_only import EmptyCell
 
 
 '''
@@ -28,75 +27,87 @@ from openpyxl.cell.read_only import EmptyCell
 
 logger = logging.getLogger()
 
+
 def value(cell):
-    return Decimal(str(cell.value))
+    return Decimal(str(cell.value or 0))
+
+
+def empty_cell(cell):
+    return cell.value is None or cell.value == ''
+
 
 def read_invoice(workbook):
-    '''文件自查'''
+    """文件自查"""
     sheet = workbook.worksheets[0]
-    max_col = sheet.max_col
     # 发票号，地址，原产国，PO号，物料号，数量，单价，合计，总数量，总合计，总件数，总毛重，总净重
     columns = None  # { '发票号': 1, '物料号': 3, ... }
     # { 'invoice_no': { 'sum': { '总合计': 1, '总毛重': 2, ... }, 'details': [{ 'PO号': 'xxx', '物料号': '', ... }, ...] } }
     invoices, all_sum = {}, None
-    for ri, row in enumerate(sheet.iter_rows()):
-        if not ri:  # 首行表头忽略
-            continue
-        if ri == 1:  # 第二行根据中文提取字段
-            columns = dict((cell.value, cell.column - 1) for cell in row if not isinstance(cell, EmptyCell))
-        else:
+    for row in sheet.iter_rows():
+        if columns:
             # 汇总行只有一行，如果有多个，则出错
-            cell_a_n = row[0]
-            if isinstance(cell_a_n, EmptyCell):
+            cell0 = row[0]
+            if not cell0.value: # '' or None
                 all_sum = {
+                    'row': row, 'errors': set(),
                     '总数量': value(row[columns['总数量']]),
                     '总合计': value(row[columns['总合计']]),
                     '总毛重': value(row[columns['总毛重']]),
-                    # NOTE: 为什么总件数，总净重没有呢？
+                    # '总件数': value(row[columns['总件数']]),
+                    # NOTE: 总件数，总净重没有
                 }
             else:
                 # 只处理所需的字段，由于涉及到空单元格的问题，因此需要逐个字段处理，如果为空则按0处理
-                detail = {}
-                for key in ('发票号', 'PO号（明细）', '物料号'):
+                errors = set()
+                detail = {'row': row, 'errors': errors}
+                for key in ('发票号', 'po号', '物料号'):
                     cell = row[columns[key]]
-                    if isinstance(cell, EmptyCell):
-                        raise Exception('%s 为空' % key)
-                    else:
+                    if cell.value:
                         detail[key] = cell.value
-                for key in ('数量', '单价', '合计', '总数量', '总合计', '总件数', '总毛重'):
-                    cell = row[columns[key]]
-                    detail[key] = Decimal(0) if isinstance(cell, EmptyCell) else value(cell)
-                invoice = invoices.setdefault(cell_a_n.value, {'details': [], 'sum': detail})
+                    else:
+                        # 是否需要单元格背景颜色改变
+                        errors.add('%s为空' % key)
+                # NOTE: 没有总净重
+                detail.update((key, value(row[columns[key]])) for key in ('数量', '单价', '合计', '总数量', '总合计', '总件数', '总毛重'))
+                # if cell.value is None or cell.value == '':
+                #     # 是否需要单元格颜色背景改变？
+                #     row_error.append('%s为空' % key)
+                # else:
+                #     detail[key] = Decimal(0) if isinstance(cell, EmptyCell) else value(cell)
+                invoice = invoices.setdefault(cell0.value, {'details': [], 'sum': detail})
                 invoice['details'].append(detail)
                 # NOTE: 浮点数计算问题不可忽略
                 if detail['合计'] != detail['数量'] * detail['单价']:
-                    raise Exception('单行 数量*单价与合计不符')
-    # 每一个发票号明细总数量全部相等，并等于该发票号所有数量合计
+                    errors.add('合计计算错误')
+                # 每一个发票号明细总数量全部相等，并等于该发票号所有数量合计
+        else:  # 首行表头根据中文提取字段
+            columns = dict((cell.value, cell.column - 1) for cell in row if cell.value)
     all_invoices = invoices.values()
     for invoice in all_invoices:
         sum1, details = invoice['sum'], invoice['details']
         total_qty, total_invoice_value, total_gross_weight, total_pkgs = sum1['总数量'], sum1['总合计'], sum1['总毛重'], sum1['总件数']
         if total_qty != sum(r['数量'] for r in details):
-            raise Exception('总数量与数量总和不符')
+            map(lambda d: d['errors'].add('总数量错误'), details)
         if not all(r['总数量'] == total_qty for r in details):
-            raise Exception('总数量错误')
+            map(lambda d: d['errors'].add('总数量错误'), details)
         if total_invoice_value != sum(r['合计'] for r in details):
-            raise Exception('总合计与合计总和不符')
+            map(lambda d: d['errors'].add('总合计错误'), details)
         if not all(r['总合计'] == total_invoice_value for r in details):
-            raise Exception('总合计错误')
+            map(lambda d: d['errors'].add('总合计错误'), details)
         # 毛重没法比对，只能和自己比对
         if not all(r['总毛重'] == total_gross_weight for r in details):
-            raise Exception('总毛重错误')
+            map(lambda d: d['errors'].add('总毛重错误'), details)
         if not all(r['总件数'] == total_pkgs for r in details):
-            raise Exception('总件数错误')
+            map(lambda d: d['errors'].add('总件数错误'), details)
     if all_sum['总数量'] != sum(v['sum']['总数量'] for v in all_invoices):
-        raise Exception('发票总数量合计错误')
+        all_sum['errors'].add('总数量汇总错误')
     if all_sum['总合计'] != sum(v['sum']['总合计'] for v in all_invoices):
-        raise Exception('发票总合计合计错误')
+        all_sum['errors'].add('总合计汇总错误')
     if all_sum['总毛重'] != sum(v['sum']['总毛重'] for v in all_invoices):
-        raise Exception('发票总毛重合计错误')
+        all_sum['errors'].add('总毛重汇总错误')
     # NOTE: 没有总件数和总净重的核对需求
     logger.info('发票文件核对完成')
+
 
 def read_packing(workbook):
     # 发票号，PO号，物料号，数量，单价，合计，总数量，总合计，总件数，总毛重，总净重
@@ -104,46 +115,42 @@ def read_packing(workbook):
     columns = None  # { '发票号': 1, '物料号': 3, ... }
     # { 'invoice_no': { 'sum': { '总合计': 1, '总毛重': 2, ... }, 'details': [{ 'PO号': 'xxx', '物料号': '', ... }, ...] } }
     packings = {}
-    for ri, row in enumerate(sheet.iter_rows()):
-        if not ri:  # 首行表头忽略
-            continue
-        if ri == 1:  # 第二行根据中文提取字段
-            columns = dict((cell.value, cell.column - 1) for cell in row if not isinstance(cell, EmptyCell))
-        else:
-            cell_a_n = row[0] # columns['发票号'] == 0
+    for row in sheet.iter_rows():
+        if columns:
             # 只处理所需的字段，由于涉及到空单元格的问题，因此需要逐个字段处理，如果为空则按0处理
-            detail = {}
-            for key in ('发票号', 'PO号', '物料号'):
+            errors = set()
+            detail = {'row': row, 'errors': errors}
+            for key in ('发票号', 'po号', '物料号'):
                 cell = row[columns[key]]
-                if isinstance(cell, EmptyCell):
-                    raise Exception('%s 为空' % key)
-                else:
+                if cell.value:
                     detail[key] = cell.value
-            for key in ('数量', '净重', '毛重', '总数量', '总净重', '总毛重', '总件数'):
-                cell = row[columns[key]]
-                detail[key] = Decimal(0) if isinstance(cell, EmptyCell) else value(cell)
-            pkg = packings.setdefault(cell_a_n.value, {'details': [], 'sum': detail})
+                else:
+                    errors.add('%s为空' % key)
+            detail.update((key, value(row[columns[key]])) for key in ('数量', '净重', '毛重', '总数量', '总净重', '总毛重', '总件数', '总托盘数', '总箱数'))
+            pkg = packings.setdefault(detail['发票号'], {'details': [], 'sum': detail})
             # NOTE 单个料单净重，毛重可能为空，所以此处比较大小没有太多意义（数据出现过某料号有净重，无毛重）
             pkg['details'].append(detail)
+        else:  # 第二行根据中文提取字段
+            columns = dict((cell.value, cell.column - 1) for cell in row if cell.value)
     # 每一个发票号明细总数量，总净重，总毛重全部相等，总件数全部相同，并等于该发票号所有数量合计
     all_pkgs = packings.values()
     for invoice in all_pkgs:
         sum1, details = invoice['sum'], invoice['details']
         total_qty, total_net_weight, total_gross_weight, total_pkgs = sum1['总数量'], sum1['总净重'], sum1['总毛重'], sum1['总件数']
         if total_qty != sum(r['数量'] for r in details):
-            raise Exception('总数量与数量总和不符')
+            map(lambda d: d['errors'].add('总数量错误'), details)
         if not all(r['总数量'] == total_qty for r in details):
-            raise Exception('总数量错误')
+            map(lambda d: d['errors'].add('总数量错误'), details)
         if total_net_weight != sum(r['净重'] for r in details):
-            raise Exception('总净重与净重总和不符')
+            map(lambda d: d['errors'].add('总净重错误'), details)
         if not all(r['总净重'] == total_net_weight for r in details):
-            raise Exception('总净重错误')
+            map(lambda d: d['errors'].add('总净重错误'), details)
         if total_gross_weight != sum(r['毛重'] for r in details):
-            raise Exception('总毛重与毛重总和不符')
+            map(lambda d: d['errors'].add('总毛重错误'), details)
         if not all(r['总毛重'] == total_gross_weight for r in details):
-            raise Exception('总毛重错误')
+            map(lambda d: d['errors'].add('总毛重错误'), details)
         if not all(r['总件数'] == total_pkgs for r in details):
-            raise Exception('总件数错误')
+            map(lambda d: d['errors'].add('总件数错误'), details)
     logger.info('箱单文件核对完成')
 
 
@@ -153,27 +160,22 @@ def read_air(workbook):
     columns = None  # { '发票号': 1, '物料号': 3, ... }
     # { 'invoice_no': { 'sum': { '总合计': 1, '总毛重': 2, ... }, 'details': [{ 'PO号': 'xxx', '物料号': '', ... }, ...] } }
     packings = {}
-    for ri, row in enumerate(sheet.iter_rows()):
-        if not ri:  # 首行表头忽略
-            continue
-        if ri == 1:  # 第二行根据中文提取字段
-            columns = dict((cell.value, cell.column - 1) for cell in row if not isinstance(cell, EmptyCell))
-        else:
-            cell_a_n = row[0] # columns['发票号'] == 0
-            # 只处理所需的字段，由于涉及到空单元格的问题，因此需要逐个字段处理，如果为空则按0处理
-            detail = {}
+    for row in sheet.iter_rows():
+        if columns:  # 首行表头忽略
+            errors = set()
+            detail = {'row': row, 'errors': errors}
             for key in ('主提运单号', '分提运单号'):
                 cell = row[columns[key]]
-                if isinstance(cell, EmptyCell):
-                    raise Exception('%s 为空' % key)
-                else:
+                if cell.value:
                     detail[key] = cell.value
-            for key in ('托盘数', '总毛重', '总件数'):
-                cell = row[columns[key]]
-                detail[key] = Decimal(0) if isinstance(cell, EmptyCell) else value(cell)
-            pkg = packings.setdefault(cell_a_n.value, {'details': [], 'sum': detail})
+                else:
+                    errors.add('%s为空' % key)
+            detail.update((key, value(row[columns[key]])) for key in ('托盘数', '总毛重', '总件数'))
+            pkg = packings.setdefault(detail['主提运单号'], {'details': [], 'sum': detail})
             pkg['details'].append(detail)
-    # 每一个发票号明细总数量，总净重，总毛重全部相等，总件数全部相同，并等于该发票号所有数量合计
+        else:  # 第二行根据中文提取字段
+            columns = dict((cell.value, cell.column - 1) for cell in row if cell.value)
+    # TODO 确认：汇总逻辑，主运单/分运单如何与箱单和发票核对
     all_pkgs = packings.values()
     for invoice in all_pkgs:
         sum1, details = invoice['sum'], invoice['details']
